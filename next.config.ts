69f302ba63829e3aca74e39ca9a48b422c45aaa2
Hang Yeung <yeungh@umich.edu>
2026-06-17 20:44:26 -0700

    Clean up

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["172.16.0.49"],
-  output: 'standalone',
+  output: "standalone",
 };
 
 export default nextConfig;
