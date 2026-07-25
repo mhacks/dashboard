56b55a3f58670b2861dfc09c8ee6ae9d9857b577
hujalex <ahuwindsor@gmail.com>
2026-07-24 22:25:46 -0400

    resolve prettier

--- app/apply/form-options.ts
+++ app/apply/form-options.ts
@@ -7,7 +7,7 @@ export const ethnicityOptions = [
   { value: "pacific-islander", label: "Native Hawaiian or Pacific Islander" },
   { value: "white", label: "White" },
   { value: "multiracial", label: "Multiracial (please describe)" },
-  { value: "prefer-not-to-say", "Prefer not to say"}
+  { value: "prefer-not-to-say", "Prefer not to say"},
 ];
 
 export const countries = [
